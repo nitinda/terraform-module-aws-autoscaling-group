1d5c4d7e267516cdb75f83401f1309e781c7e72a
nitin-das <nitin.das@thomson.co.uk>
2020-04-08 19:24:26 +0100

    Modified Code

--- autoscaling-group.tf
+++ autoscaling-group.tf
@@ -1,28 +1,13 @@
 resource "aws_autoscaling_group" "autoscaling_group" {
-  name_prefix               = var.name_prefix
-  max_size                  = var.max_size
-  min_size                  = var.min_size
-  desired_capacity          = var.desired_capacity
-  vpc_zone_identifier       = var.vpc_zone_identifier
-  health_check_type         = var.health_check_type
-  health_check_grace_period = var.health_check_grace_period
-  default_cooldown          = var.default_cooldown
-  force_delete              = var.force_delete
-  suspended_processes       = var.suspended_processes
-  target_group_arns         = var.target_group_arns
-  load_balancers            = var.load_balancers
-
-  dynamic "tag" {
-    for_each = var.tags
-    content {
-      key                 = tag.key
-      value               = tag.value
-      propagate_at_launch = true
-    }
-  }
+  name                 = var.name
+  name_prefix          = var.name_prefix
+  max_size             = var.max_size
+  min_size             = var.min_size
+  default_cooldown     = var.default_cooldown
+  launch_configuration = var.launch_configuration
 
   dynamic "launch_template" {
-    for_each = length(keys(var.launch_template)) == 0 ? [] : [var.launch_template]
+    for_each = var.launch_template
     content {
       id      = lookup(launch_template.value, "id", null)
       name    = lookup(launch_template.value, "name", null)
@@ -31,10 +16,10 @@ resource "aws_autoscaling_group" "autoscaling_group" {
   }
 
   dynamic "mixed_instances_policy" {
-    for_each = var.mixed_instances_policy == {} ? [] : [var.mixed_instances_policy]
+    for_each = var.mixed_instances_policy
     content {
       dynamic "instances_distribution" {
-        for_each = length(keys(lookup(mixed_instances_policy.value, "instances_distribution", {}))) == 0 ? [] : [lookup(mixed_instances_policy.value, "instances_distribution", {})]
+        for_each = lookup(mixed_instances_policy.value, "instances_distribution", [])
         content {
           on_demand_allocation_strategy            = lookup(instances_distribution.value, "on_demand_allocation_strategy", null)
           on_demand_base_capacity                  = lookup(instances_distribution.value, "on_demand_base_capacity", null)
@@ -46,10 +31,10 @@ resource "aws_autoscaling_group" "autoscaling_group" {
       }
 
       dynamic "launch_template" {
-        for_each = length(keys(lookup(mixed_instances_policy.value, "launch_template", {}))) == 0 ? [] : [lookup(mixed_instances_policy.value, "launch_template", {})]
+        for_each = lookup(mixed_instances_policy.value, "launch_template", [])
         content {
           dynamic "launch_template_specification" {
-            for_each = length(keys(lookup(launch_template.value, "launch_template_specification", {}))) == 0 ? [] : [lookup(launch_template.value, "launch_template_specification", {})]
+            for_each = lookup(launch_template.value, "launch_template_specification", [])
             content {
               launch_template_id   = lookup(launch_template_specification.value, "launch_template_id", null)
               launch_template_name = lookup(launch_template_specification.value, "launch_template_name", null)
@@ -58,7 +43,7 @@ resource "aws_autoscaling_group" "autoscaling_group" {
           }
 
           dynamic "override" {
-            for_each = length(lookup(launch_template.value, "override", [])) == 0 ? [] : lookup(launch_template.value, "override", [])
+            for_each = lookup(launch_template.value, "override", [])
             content {
               instance_type     = lookup(override.value, "instance_type", null)
               weighted_capacity = lookup(override.value, "weighted_capacity", null)
@@ -69,9 +54,44 @@ resource "aws_autoscaling_group" "autoscaling_group" {
     }
   }
 
+  dynamic "initial_lifecycle_hook" {
+    for_each = var.initial_lifecycle_hook
+    content {
+      default_result          = lookup(initial_lifecycle_hook.value, "default_result", null)
+      heartbeat_timeout       = lookup(initial_lifecycle_hook.value, "heartbeat_timeout", null)
+      lifecycle_transition    = initial_lifecycle_hook.value.lifecycle_transition
+      name                    = initial_lifecycle_hook.value.name
+      notification_metadata   = lookup(initial_lifecycle_hook.value, "notification_metadata", null)
+      notification_target_arn = lookup(initial_lifecycle_hook.value, "notification_target_arn", null)
+      role_arn                = lookup(initial_lifecycle_hook.value, "role_arn", null)
+    }
+  }
+
+  health_check_grace_period = var.health_check_grace_period
+  health_check_type         = var.health_check_type
+  desired_capacity          = var.desired_capacity
+  force_delete              = var.force_delete
+  load_balancers            = var.load_balancers
+  vpc_zone_identifier       = var.vpc_zone_identifier
+  target_group_arns         = var.target_group_arns
+  termination_policies      = var.termination_policies
+  suspended_processes       = var.suspended_processes
+  metrics_granularity       = var.metrics_granularity
+  enabled_metrics           = var.enabled_metrics
+
+  dynamic "tag" {
+    for_each = var.tag
+    content {
+      key                 = tag.value.key
+      propagate_at_launch = tag.value.propagate_at_launch
+      value               = tag.value.value
+    }
+  }
+
+  tags            = var.tags
+  placement_group = var.placement_group
+
   lifecycle {
     create_before_destroy = true
   }
-
 }
-
--- variables.tf
+++ variables.tf
@@ -1,18 +1,55 @@
 ## Auto Scalling Group
-variable "name_prefix" {
-  description = "Creates a unique name beginning with the specified prefix."
+variable "name" {
+  description = "The name of the auto scaling group"
+  default     = null
 }
 
-variable "desired_capacity" {
-  description = "The number of Amazon EC2 instances that should be running in the group."
+variable "name_prefix" {
+  description = "Creates a unique name beginning with the specified prefix"
+  default     = null
 }
 
 variable "max_size" {
-  description = "The maximum size of the auto scale group."
+  description = "The maximum size of the auto scale group"
 }
 
 variable "min_size" {
-  description = "The minimum size of the auto scale group."
+  description = "The minimum size of the auto scale group"
+}
+
+variable "default_cooldown" {
+  description = "The amount of time, in seconds, after a scaling activity completes before another scaling activity can start."
+  type        = number
+  default     = 60
+}
+
+variable "launch_configuration" {
+  description = "The name of the launch configuration to use"
+  default     = null
+}
+
+variable "launch_template" {
+  description = "Nested argument with Launch template specification to use to launch instances."
+  type        = any
+  default     = []
+}
+
+variable "mixed_instances_policy" {
+  description = "Configuration block containing settings to define launch targets for Auto Scaling groups"
+  default     = []
+  type        = any
+}
+
+variable "initial_lifecycle_hook" {
+  description = "One or more Lifecycle Hooks to attach to the autoscaling group before instances are launched"
+  default     = []
+  type        = any
+}
+
+variable "health_check_grace_period" {
+  description = "Time (in seconds) after instance comes into service before checking health. Default: 300"
+  default     = 300
+  type        = number
 }
 
 variable "health_check_type" {
@@ -20,15 +57,16 @@ variable "health_check_type" {
   default     = "EC2"
 }
 
-variable "vpc_zone_identifier" {
-  description = "A list of subnet IDs to launch resources in."
-  type        = list(string)
+variable "desired_capacity" {
+  description = "The number of Amazon EC2 instances that should be running in the group."
+  default     = 0
+  type        = number
 }
 
-variable "target_group_arns" {
-  description = "A list of aws_alb_target_group ARNs"
-  type        = list(string)
-  default     = []
+variable "force_delete" {
+  description = "Allows deleting the autoscaling group without waiting for all instances in the pool to terminate"
+  default     = true
+  type        = bool
 }
 
 variable "load_balancers" {
@@ -37,19 +75,22 @@ variable "load_balancers" {
   default     = []
 }
 
-variable "default_cooldown" {
-  description = "The amount of time, in seconds, after a scaling activity completes before another scaling activity can start."
-  default     = 60
+variable "vpc_zone_identifier" {
+  description = "A list of subnet IDs to launch resources in."
+  type        = list(string)
+  default     = []
 }
 
-variable "force_delete" {
-  description = "Allows deleting the autoscaling group without waiting for all instances in the pool to terminate"
-  default     = true
+variable "target_group_arns" {
+  description = "A list of aws_alb_target_group ARNs"
+  type        = list(string)
+  default     = []
 }
 
-variable "health_check_grace_period" {
-  description = "Time (in seconds) after instance comes into service before checking health. Default: 300"
-  default     = 300
+variable "termination_policies" {
+  description = "A list of policies to decide how the instances in the auto scale group should be terminated"
+  default     = []
+  type        = list(string)
 }
 
 variable "suspended_processes" {
@@ -63,26 +104,7 @@ variable "tags" {
   type        = map(string)
 }
 
-variable "launch_template" {
-  description = "Nested argument with Launch template specification to use to launch instances."
-  type        = map(string)
+variable "placement_group" {
+  description = "The name of the placement group into which you'll launch your instances, if any"
+  default     = null
 }
-
-variable "mixed_instances_policy" {
-  description = "Configuration block containing settings to define launch targets for Auto Scaling groups"
-  default     = {}
-  type        = any
-}
-
-# variable "override_instance_types" {
-#   description = "The size of instance to launch, minimum 2 types must be specified."
-#   type        = list(string)
-# }
-# variable "launch_template_specification" {
-#   description = "Nested argument defines the Launch Template."
-#   type        = list(string)
-# }
-# variable "instances_distribution" {
-#   description = "Nested argument containing settings on how to mix on-demand and Spot instances in the Auto Scaling group."
-#   type        = list(string)
-# }
